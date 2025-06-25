c221b993a38ce45d8c8485b0b79509895b1e793b
twcult <delyfss@proton.me>
2025-06-25 17:53:00 +0300

    add list

--- src/main/kotlin/me/delyfss/cocal/Config.kt
+++ src/main/kotlin/me/delyfss/cocal/Config.kt
@@ -15,7 +15,7 @@ class Config<T : Any>(
         val defaultText = generateDefaults()
         val defaultConfig = ConfigFactory.parseString(defaultText)
 
-        val existingConfig = if (file.exists() && file.readText().isBlank()) {
+        val existingConfig = if (file.exists() && file.readText().isNotBlank()) {
             try {
                 ConfigFactory.parseFile(file)
             } catch (e: ConfigException) {
@@ -31,12 +31,25 @@ class Config<T : Any>(
             val annotation = field.getDeclaredAnnotation(Path::class.java) ?: continue
             val path = annotation.value
             field.isAccessible = true
-            when (field.type) {
-                Boolean::class.javaPrimitiveType -> field.setBoolean(target, merged.getBoolean(path))
-                Int::class.javaPrimitiveType -> field.setInt(target, merged.getInt(path))
-                Long::class.javaPrimitiveType -> field.setLong(target, merged.getLong(path))
-                Double::class.javaPrimitiveType -> field.setDouble(target, merged.getDouble(path))
-                String::class.java -> field.set(target, merged.getString(path))
+
+            when {
+                field.type == Boolean::class.javaPrimitiveType -> field.setBoolean(target, merged.getBoolean(path))
+                field.type == Int::class.javaPrimitiveType -> field.setInt(target, merged.getInt(path))
+                field.type == Long::class.javaPrimitiveType -> field.setLong(target, merged.getLong(path))
+                field.type == Double::class.javaPrimitiveType -> field.setDouble(target, merged.getDouble(path))
+                field.type == String::class.java -> field.set(target, merged.getString(path))
+                List::class.java.isAssignableFrom(field.type) -> {
+                    val typeStr = field.genericType.toString()
+                    val list = when {
+                        "String" in typeStr -> merged.getStringList(path)
+                        "Integer" in typeStr || "Int" in typeStr -> merged.getIntList(path)
+                        "Boolean" in typeStr -> merged.getBooleanList(path)
+                        "Double" in typeStr -> merged.getDoubleList(path)
+                        "Long" in typeStr -> merged.getLongList(path)
+                        else -> error("Unsupported list type for path '$path'")
+                    }
+                    field.set(target, list)
+                }
                 else -> error("Unsupported config type for path '$path'")
             }
         }
@@ -60,6 +73,14 @@ class Config<T : Any>(
             "${ann.value} = ${formatValue(it.get(target))}"
         }.joinToString("\n")
 
-    private fun formatValue(value: Any?): String =
-        if (value is String) "\"$value\"" else value.toString()
+    private fun formatValue(value: Any?): String = when (value) {
+        is String -> "\"$value\""
+        is List<*> -> value.joinToString(prefix = "[", postfix = "]") {
+            when (it) {
+                is String -> "\"$it\""
+                else -> it.toString()
+            }
+        }
+        else -> value.toString()
+    }
 }
